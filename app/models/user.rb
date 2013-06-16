1b3b663e9806c532c9ce5d617b9217c9dbc936c9
Weiying Yu <awencye@gmail.com>
2013-06-16 12:37:56 -0400

    Authentication

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+  include BCrypt
+  attr_accessible :email, :password, :password_confirmation
+  
+  attr_accessor :password
+  before_save :encrypt_password
+  
+  validates :password, :confirmation => true, :presence => { :on => :create }
+  validates :email, :uniqueness => true, :presence => true
+  
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+  
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Shouter::Application.routes.draw do
-  get "shout/index"
+  
+get "log_out" => "sessions#destroy", :as => "log_out"
+get "log_in" => "sessions#new", :as => "log_in"
+get "sign_up" => "users#new", :as => "sign_up"
+root :to => "users#new"
+resources :users
+resources :sessions
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
